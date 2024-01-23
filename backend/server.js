02e7895513515f33827be7368546b35268e57b02
Rostislav Goncharov <rgoncharov2010@gmail.com>
2024-01-23 11:22:16 +0200

    add request logger

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,15 @@ const express = require('express');
 const app = express();
 const PORT = 7777;
 
+app.use((req, res, next) => {
+    console.log(req.method, req.path);
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.json({'mssg': 'Hi'});
+});
+
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
