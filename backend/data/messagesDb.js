3d6f7301838bfcc25922be24b91d98947fc16d70
Rostislav Goncharov <rgoncharov2010@gmail.com>
2024-01-27 13:46:50 +0200

    add id property to messages

--- backend/data/messagesDb.js
+++ backend/data/messagesDb.js
@@ -15,14 +15,17 @@ let channels = [
 
 let messages = [
     {
+        id: 0,
         channelId: 0,
         text: 'Message from Channel 1',
     },
     {
+        id: 1,
         channelId: 1,
         text: 'Message from Channel 2',
     },
     {
+        id: 2,
         channelId: 2,
         text: 'Message from Channel 3',
     },
