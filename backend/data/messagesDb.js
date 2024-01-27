b9ff28f47114596ad1c1e10b36f629410aaf0f2d
Rostislav Goncharov <rgoncharov2010@gmail.com>
2024-01-27 12:57:47 +0200

    remove unused code

--- backend/data/messagesDb.js
+++ backend/data/messagesDb.js
@@ -1,21 +1,3 @@
-// let database = [
-//     {
-//         channelId: 0,
-//         channelName: 'Channel 1',
-//         messages: [],
-//     },
-//     {
-//         channelId: 1,
-//         channelName: 'Channel 2',
-//         messages: [],
-//     },
-//     {
-//         channelId: 2,
-//         channelName: 'Channel 3',
-//         messages: [],
-//     },
-// ];
-
 let channels = [
     {
         id: 0,
