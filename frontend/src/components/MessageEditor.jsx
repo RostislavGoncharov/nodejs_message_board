210aab915395c790ab2f9052963b72d08121812b
Rostislav Goncharov <rgoncharov2010@gmail.com>
2024-01-28 22:48:14 +0200

    implement MessageEditor component

---
+++ frontend/src/components/MessageEditor.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+const MessageEditor = ({messages, currentChannelId, setMessages}) => {
+    const [newMessage, setNewMessage] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const message = { id: 0 + messages.messages.length, channelId: currentChannelId, text: newMessage };
+        setNewMessage('');
+    
+        await fetch(`http://localhost:7777/${currentChannelId}`, {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify(message),
+        });
+    
+        setMessages({messages: [...messages.messages, message]});
+      }
+
+    return (
+        <div className="messageEditorPanel">
+          <form onSubmit={handleSubmit} action="">
+              <label htmlFor="messageInput">New Message: </label>
+              <textarea id="messageInput" onChange={(e) => {setNewMessage(e.target.value)}}></textarea>
+              {newMessage.trim() != '' && <input type="submit" value="Post Message"/>}
+          </form>
+        </div>
+    );
+};
+
+export default MessageEditor;
